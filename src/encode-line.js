e5ea2ff9837771c5466e08207e117ca7f4ccacb2
vladislav-garaev <marker661@gmail.com>
2023-04-15 11:37:37 +0300

    feat: get-email-domain done (Sat Apr 15 11:37:36 +03 2023)

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  let arr = [...str]
+  let result = ''
+  for (let i = 0; i < arr.length; ++i) {
+    if(arr[i] === arr[i+1]) {
+      arr[i] 
+    }
+  }
+  return result
 }
 
 module.exports = {
