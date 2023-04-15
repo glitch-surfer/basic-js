0f12865920da072bad2569837ba072c3b9ed1c6c
vladislav-garaev <marker661@gmail.com>
2023-04-15 16:12:17 +0300

    feat: done simple=chain (Sat Apr 15 16:12:17 +03 2023)

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,43 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  count: 0,
+  arr: [],
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.count
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  addLink(value) {
+      this.arr.push(`( ${value} )`)
+      this.count++
+      return this
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  removeLink(position) {
+    if (typeof position !== 'number'
+    || position <= 0 
+    || position > this.arr.length - 1) {
+      this.arr = []
+      this.count = 0
+      throw new Error("You can't remove incorrect link!")
+    }
+    this.arr.splice(position - 1, 1)
+    this.count--
+    return this
   },
+
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.arr.reverse()
+    return this
   },
+
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let result = this.arr.join('~~')
+    this.arr = []
+    this.count = 0
+
+    return result
   }
 };
 
